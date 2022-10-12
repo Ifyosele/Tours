a4e4596b73b2d033a27398d8acb9d09a09cccd38
Osele Ify <onyeneifeyinwa30@gmail.com>
2022-10-12 16:06:01 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ function App() {
   const [tours, setTours] = useState([]);
 
 
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+
   const fetchTours = async () => {
     setLoading(true);
 
@@ -36,9 +43,20 @@ function App() {
       </main>
     );
   }
+
+   if (tours.length=== 0) {
+      return (
+        <main>
+          <div className='title'>
+            <h2>no tours left</h2>
+            <button className='btn' onClick={fetchTours}>refresh</button>
+          </div>
+        </main>
+      );
+   }
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
